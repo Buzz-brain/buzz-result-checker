9a348de8e319b15097616e32007fcd10c837b4c2
unknown <Buzz-brain>
2024-10-31 16:08:23 +0100

    Get models through cdn

--- public/js/facial_registration.js
+++ public/js/facial_registration.js
@@ -3,11 +3,17 @@ console.log("Loading face-api.js models...");
 const video = document.getElementById('video');
 
 Promise.all([
+    await faceapi.nets.tinyFaceDetector.loadFromUri('https://cdn.jsdelivr.net/npm/@vladmandic/face-api/model/tiny_face_detector_model-weights_manifest.json'),
+    await faceapi.nets.faceLandmark68Net.loadFromUri('https://cdn.jsdelivr.net/npm/@vladmandic/face-api/model/face_landmark_68_model-weights_manifest.json'),
+    await faceapi.nets.faceRecognitionNet.loadFromUri('https://cdn.jsdelivr.net/npm/@vladmandic/face-api/model/face_recognition_model-weights_manifest.json'),
+    await faceapi.nets.faceExpressionNet.loadFromUri('https://cdn.jsdelivr.net/npm/@vladmandic/face-api/model/face_expression_model-weights_manifest.json')
+
+    // Get models locally
     // faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
-    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-    faceapi.nets.faceExpressionNet.loadFromUri('/models')
+    // faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    // faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    // faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    // faceapi.nets.faceExpressionNet.loadFromUri('/models')
 ]).then(() => {
     console.log("Models loaded successfully.");
     startVideo();
